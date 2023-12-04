54f888bbc03d51e97d443f842fbbc67aaea94344
prasanth.m@sedintechnologies.com <prasanth.m@sedintechnologies.com>
2023-12-04 16:02:26 +0530

    New commit after rejection

---
+++ tests/todo/create1.spec.ts
@@ -0,0 +1,11 @@
+import {  expect } from '@playwright/test';
+import { login } from '../../testdata/user';
+import { test } from '../../fixture/user.fixture'
+import { TODO_RESOURCE } from '../../config';
+test('creation of todo should work', async ({ authenticatedRequest }) => {
+  const resp = await authenticatedRequest.post(TODO_RESOURCE,{
+    title:'Bring Milk'
+  })
+  expect(resp.status()).toBe(201)
+});
+//New file created
