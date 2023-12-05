b03746927b8485db22544b8e3cae454b5309c5b1
prasanth.m@sedintechnologies.com <prasanth.m@sedintechnologies.com>
2023-12-05 11:27:37 +0530

    combined changes

--- tests/todo/testbranchfile.spec.ts
+++ tests/todo/testbranchfile.spec.ts
@@ -1,17 +1,8 @@
 //newbranch file creation
 //file creation on same file for trunk based development
-
+//One more time trunk base resolve
 console.log("New branch created");
-
-
-
-
-
 console.log("1-2-3-4");
-
-
-
-
-
-
 //line 10 updated succesfully
+//line 10 update
+//line 18 update
